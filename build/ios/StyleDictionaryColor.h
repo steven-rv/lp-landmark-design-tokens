6087b4151f38b2c03e8877cf96fca33bd0b712b6
Automated Publisher <actions@users.noreply.github.com>
2021-06-29 20:53:58 +0000

    Automated publish: Tue Jun 29 20:53:58 UTC 2021 a286da1782add074286aa43c6db26fbeb42a1afc

---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,43 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly
+// Generated on Tue, 29 Jun 2021 20:53:57 GMT
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+BordersUnsupportedMultipleBordersStroke,
+BordersSingleStroke,
+BordersSingleStyleStroke,
+BordersDashedOutsideStroke,
+ColorsMultipleFills0,
+ColorsMultipleFills1,
+ColorsSingleBlue,
+ColorsSpecialCharacters,
+ColorsSpecialCharactersNderung,
+GradientMultiple0Stops0Color,
+GradientMultiple0Stops1Color,
+GradientMultiple1Stops0Color,
+GradientMultiple1Stops1Color,
+GradientMultiple2Stops0Color,
+GradientMultiple2Stops1Color,
+GradientMultiple3Stops0Color,
+GradientMultiple3Stops1Color,
+GradientSingleWithMultipleColorStopsStops0Color,
+GradientSingleWithMultipleColorStopsStops1Color,
+GradientSingleWithMultipleColorStopsStops2Color,
+GradientSingleWithMultipleColorStopsStops3Color,
+EffectDropShadowSingleColor,
+EffectInnerShadowMultiple0Color,
+EffectInnerShadowMultiple1Color,
+EffectInnerShadowMultiple2Color
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
